c6f61f94db8703536fdfc31f1fc263e484e511e2
rebeldynamo <95393797+rebeldynamo@users.noreply.github.com>
2024-02-25 16:50:37 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   // site: "https://loglosslabs.com",
-  site: "loglosslabs.github.io",
+  site: "https://loglosslabs.github.io",
   integrations: [tailwind()],
 });
